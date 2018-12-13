ebe859f15df5b1e3d13509ddf83f218fd72fffec
Simbarashe Timothy Motsi <simbamotsi1@gmail.com>
2018-12-13 15:12:08 +0200

    removed sphinx bootstrap

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
-import sphinx_bootstrap_theme
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
-# Activate the theme.
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # -- Project information -----------------------------------------------------
 
